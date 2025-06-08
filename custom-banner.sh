78e8141572ad34f8617ba94139c62d537f4546d3
Fc.Sp@rt@k <roma25kovrov@mail.ru>
2025-06-08 15:39:54 +0300

    Update custom-banner.sh

--- custom-banner.sh
+++ custom-banner.sh
@@ -83,7 +83,7 @@ printf "   ${wht} %-10s ${grn} %-30s ${wht} %-10s ${grn} %-30s ${clr}\n" \
     "Architecture:" "$(uname -m)"
 printf "   ${wht} %-10s ${grn} %-30s ${wht} %-10s ${grn} %-30s ${clr}\n" \
     "CPU Temp:" "🌡 $(echo $THERMAL)℃" \
-    "Coress:" "     $(grep -c ^processor /proc/cpuinfo 2>/dev/null || echo '?')"
+    "Coress:" "$(grep -c ^processor /proc/cpuinfo 2>/dev/null || echo '?')"
 printf "   ${wht} %-10s ${pur} %-30s ${clr}\n" \
     "Disk:" "$(df -h | grep '/opt' | awk '{print $2" (size) / "$3" (used) / "$4" (free) / "$5" (used %) : 💾 "$6}')"
 printf "   ${wht} %-10s ${pur} %-30s ${clr}\n" \
